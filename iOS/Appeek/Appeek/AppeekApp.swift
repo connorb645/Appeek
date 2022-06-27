e17ff8dac2efe08efb66ff2b4c8ffd567beeae47
Connor Black <connor.b645@gmail.com>
2022-06-27 12:44:55 +0100

    Added base iOS project

---
+++ iOS/Appeek/Appeek/AppeekApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppeekApp.swift
+//  Appeek
+//
+//  Created by Connor Black on 27/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct AppeekApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
